fd7b8a51af56c12481a811805cb78059dcdd98a4
chenyinjie0810 <chenyinjie0810@qq.com>
2019-07-14 20:09:26 +0800

    成功

---
+++ springCloud/config3355/src/main/java/com/chenyj/Config3355Application.java
@@ -0,0 +1,17 @@
+package com.chenyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class Config3355Application
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Config3355Application.class, args);
+    }
+}
---
+++ springCloud/config3355/src/main/java/com/chenyj/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.chenyj.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author chenyj
+ * @Description
+ * @Date create by 2019/7/14 19:53
+ * 陈银杰专属测试
+ */
+@RestController
+public class TestController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String eurekaServers;
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/config")
+    public String getConfig()
+    {
+        String str = "applicationName: " + applicationName + "\t eurekaServers:" + eurekaServers + "\t port: " + port;
+        System.out.println("******str: " + str);
+        return "applicationName: " + applicationName + "\t eurekaServers:" + eurekaServers + "\t port: " + port;
+    }
+}
